af8edab563019a3d439ff02afeda53cca9c18680
malakshaer <shaermk06@gmail.com>
2022-09-10 12:34:35 +0300

    function get data is added

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-const apikey = "UelJzt3GJ8LnbYhxf9gspQLLPt8PAjcVMe4J";
-
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-const url = (name) => `https://gender-api.com/get?name=${name}&key=${apikey}`;
+const url1 = (name) => ` https://api.agify.io/?name=${name}`;
+const url2 = (name) => ` https://api.genderize.io?name=r${name}`;
+const url3 = (name) => ` https://api.nationalize.io/?name=${name}`;
 
-async function getGenderByName(name) {
-  const resp = await fetch(url(name), { origin: "cors" });
-  const respData = await resp.json();
+async function getData(name) {
+  const resp1 = await fetch(url1(name), { origin: "cors" });
+  const respData1 = await resp1.json();
 
-  console.log(respData);
+  const resp2 = await fetch(url2(name), { origin: "cors" });
+  const respData2 = await resp2.json();
 
-  addGenderToPage(respData);
+  const resp3 = await fetch(url3(name), { origin: "cors" });
+  const respData3 = await resp3.json();
+
+  console.log(respData1);
+  console.log(respData2);
+  console.log(respData3);
+
+  addAgeToPage(respData1);
+  addGenderToPage(respData2);
+  addNationalityToPage(respData3);
+
+  //clean up
+  //main.innerHTML = "";
+}
+
+function addAgeToPage(data) {
+  const theAge = document.createElement("div");
+  theAge.classList.add("info");
+
+  theAge.innerHTML = `<h2>Age: ${data.age}</h2>`;
+
+  main.appendChild(theAge);
 }
 
 function addGenderToPage(data) {
-  const gender = document.getElementById("gender");
-  gender.classList.add("info");
+  const theGender = document.createElement("div");
+  theGender.classList.add("info");
 
-  gender.innerHTML = `<h2>${data.gender}</h2>`;
+  theGender.innerHTML = `<h2> Gender: ${data.gender}</h2>`;
 
-  //clean up
-  main.innerHTML = "";
+  main.appendChild(theGender);
+}
 
-  main.appendChild(gender);
+function addNationalityToPage(data) {
+  const theNationality = document.createElement("div");
+  theNationality.classList.add("info");
+
+  theNationality.innerHTML = `<h2>Nationality1: ${data.country[0].country_id}  Nationality2: ${data.country[1].country_id}</h2>`;
+
+  main.appendChild(theNationality);
 }
 
 form.addEventListener("submit", (e) => {
@@ -33,6 +61,6 @@ form.addEventListener("submit", (e) => {
   const name = search.value;
 
   if (name) {
-    getGenderByName(name);
+    getData(name);
   }
 });
